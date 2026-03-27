a5efd1f4d4d4ee28e2d7aae0377251047cc1acd0
Paulo Dias <paulodiasads@gmail.com>
2026-03-26 21:50:29 -0300

    BUGFIX: added vercel url to CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:4200', ''],
+    origin: ['http://localhost:4200', 'https://mytasks-chi.vercel.app/'],
     credentials: true,
   }),
 );
